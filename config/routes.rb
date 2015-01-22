3cd3ca62a6f468933727ce1a36f2749052f271a3
nicholas <nicholas@sophia.lan>
2015-01-22 21:54:04 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  resources :microposts
+
+  resources :users
+  root 'static_pages#home'
+
+end
